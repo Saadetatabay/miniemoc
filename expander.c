abf985fb3e0c2690e7e8aa94d389f5d921635e47
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-14 00:10:46 +0300

    feat: implement expand_token function and var_name utility for token expansion

--- expander.c
+++ expander.c
@@ -13,3 +13,22 @@ void	expander(t_token **token_list, t_env *env_list)
 		temp = temp->next;
 	}
 }
+
+void	expand_token(t_token *temp, t_env *env_list)
+{
+
+}
+
+char	*var_name(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[0])
+		return (ft_strdup("?"));
+	if (ft_isdigit(s[0]))
+		return (ft_substr(s, 0, 1));
+	while (s[i] && (ft_isalnum(s[i]) || s[i] == '_'))
+		i++;
+	return (ft_substr(s, 0,i));
+}
